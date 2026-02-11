c3f0e99de78d52a859ded102a558cd777302640a
ritikraj4601-cyber <ritikraj4601@gmail.com>
2026-02-11 14:04:16 +0530

    Delete .app.py

--- .app.py
+++
@@ -1,7 +0,0 @@
-# ... (rest of app.py)
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
