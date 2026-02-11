c96c87aea59fafe3c048ddb24b4fea120cb3bdaf
ritikraj4601-cyber <ritikraj4601@gmail.com>
2026-02-11 14:04:49 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'fallback-key-change-this'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://...'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'fallback-jwt-key-change-this'
-    SCHEDULER_API_ENABLED = True
